ae0ebce3a640ae92846e3d741534c76eb182f349
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-11-17 11:44:33 -0300

    feat: Header created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+import "../styles/header.scss"
+
 export function Header(){
   return (
     <header>
-      <h2>Best Series</h2>
+      <a href="#">
+       <h2>Best Series</h2>
+      </a>
+      <button className="createNew">Create new</button>
     </header>
   )
 }
