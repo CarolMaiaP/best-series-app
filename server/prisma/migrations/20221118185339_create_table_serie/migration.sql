68613cc1a34a0aba805cd69265a730dac426e9df
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-11-18 15:58:33 -0300

    feat: server initialized

---
+++ server/prisma/migrations/20221118185339_create_table_serie/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Serie" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "Season" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
